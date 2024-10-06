5e96939f30d3316c7799ba4a9776d97ada98f83d
ksh <aiostpch@naver.com>
2024-10-06 13:21:08 +0900

    롬복적용
    롬복적용

--- src/main/java/db/dto/FavoritesDTO.java
+++ src/main/java/db/dto/FavoritesDTO.java
@@ -1,28 +1,14 @@
 package db.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class FavoritesDTO {
 	
 	private int number;
 	private String name;
 	private String type;
-	
-	public int getNumber() {
-		return number;
-	}
-	public void setNumber(int number) {
-		this.number = number;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
 	
 }
--- src/main/java/db/dto/TodoDTO.java
+++ src/main/java/db/dto/TodoDTO.java
@@ -1,7 +1,13 @@
 package db.dto;
 
 import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Date;
+
+@Setter
+@Getter
 public class TodoDTO {
 	private int todo_pk_num; //일정 고유 넘버
 	private int todo_fk_user_num; //일정을 작성한 유저의 넘버
@@ -9,43 +15,5 @@ public class TodoDTO {
 	private String todo_content; //일정 내용
 	private Date todo_start; //일정 시작 날짜
 	private Date todo_end; //일정 끝 날짜
-	
-	public int getTodo_pk_num() {
-		return todo_pk_num;
-	}
-	public void setTodo_pk_num(int todo_pk_num) {
-		this.todo_pk_num = todo_pk_num;
-	}
-	public int getTodo_fk_user_num() {
-		return todo_fk_user_num;
-	}
-	public void setTodo_fk_user_num(int todo_fk_user_num) {
-		this.todo_fk_user_num = todo_fk_user_num;
-	}
-	public String getTodo_title() {
-		return todo_title;
-	}
-	public void setTodo_title(String todo_title) {
-		this.todo_title = todo_title;
-	}
-	public String getTodo_content() {
-		return todo_content;
-	}
-	public void setTodo_content(String todo_content) {
-		this.todo_content = todo_content;
-	}
-	public Date getTodo_start() {
-		return todo_start;
-	}
-	public void setTodo_start(Date todo_start) {
-		this.todo_start = todo_start;
-	}
-	public Date getTodo_end() {
-		return todo_end;
-	}
-	public void setTodo_end(Date todo_end) {
-		this.todo_end = todo_end;
-	}
-	
-	
+		
 }
