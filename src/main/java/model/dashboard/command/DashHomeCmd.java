44d6b9204f3bf397b949f3e62065ce5b2f5cbe6a
ksh <aiostpch@naver.com>
2024-10-06 14:37:35 +0900

    main
    main

--- src/main/java/model/dashboard/command/DashHomeCmd.java
+++ src/main/java/model/dashboard/command/DashHomeCmd.java
@@ -1,6 +1,8 @@
 package model.dashboard.command;
 
 import model.Icommand;
+import model.dashboard.Free_List;
+import model.dashboard.Proj_List;
 import model.dashboard.Todo_List;
 
 import javax.servlet.ServletException;
@@ -20,6 +22,16 @@ public class DashHomeCmd implements Icommand {
     	Todo_List todo = new Todo_List();
     	ArrayList todoList = todo.getTodoList(sessionID);
     	req.setAttribute("todoList", todoList);
+    	
+    	//프로젝트 목록
+		Proj_List proj = new Proj_List();
+		ArrayList projList = proj.getPost(sessionID, null, null);
+		req.setAttribute("projList", projList);
+		
+		//게시판 목록
+		Free_List post = new Free_List();
+		ArrayList postList = post.getPost(sessionID, null, null);
+		req.setAttribute("postList", postList);
     	
         return "/WEB-INF/views/dashboard/dashboard.jsp";
     }
